082e105cbfe1429f7ddafa7119f16bd297d25cd5
lars0395 <larsfrauenrath@web.de>
2018-07-03 20:32:42 +0200

    multiple requestparams

--- src/main/java/de/lars0395/github/controller/CassandraController.java
+++ src/main/java/de/lars0395/github/controller/CassandraController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -22,20 +23,30 @@ public class CassandraController {
     @RequestMapping(value = "/getData/{correlationID}", method = RequestMethod.GET)
     public List<CassandraEntry> getCassandraDataForCorrelationId(@PathVariable String correlationID) {
         ResultSet resultSet = cassandraService.getCassandraDataForCorrelationId(correlationID,null);
-        List<CassandraEntry> entries = new ArrayList<CassandraEntry>();
-        for(Row r : resultSet.all()) {
-          CassandraEntry entry = new CassandraEntry();
-          entry.setKey(r.getString("key"));
-          entry.setTimestamp(r.getTimestamp("timestamp"));
-          entry.setMetadata(r.getString("system.blobastext(metadata)"));
-          entry.setPayload(r.getString("system.blobastext(payload)"));
-          entries.add(entry);
-        }
-        return entries;
+        return createListFromResultSet(resultSet);
+    }
+
+    @RequestMapping(value = "/getData", method = RequestMethod.GET)
+    public List<CassandraEntry> getDataForIdAndIp(@RequestParam("correlationID") String correlationID, @RequestParam("ip") String ip) {
+        ResultSet resultSet = cassandraService.getCassandraDataForCorrelationId(correlationID,ip);
+        return createListFromResultSet(resultSet);
     }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String goToHomePage() {
         return "index";
+    }
+
+    private List<CassandraEntry> createListFromResultSet(ResultSet resultSet) {
+        List<CassandraEntry> entries = new ArrayList<CassandraEntry>();
+        for(Row r : resultSet.all()) {
+            CassandraEntry entry = new CassandraEntry();
+            entry.setKey(r.getString("key"));
+            entry.setTimestamp(r.getTimestamp("timestamp"));
+            entry.setMetadata(r.getString("system.blobastext(metadata)"));
+            entry.setPayload(r.getString("system.blobastext(payload)"));
+            entries.add(entry);
+        }
+        return entries;
     }
 }
